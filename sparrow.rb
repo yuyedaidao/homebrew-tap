57a26cf7e910dda83398ada36ee27484da98afd5
WYQ <wyqpadding@gmail.com>
2022-04-15 17:15:40 +0800

    add sparrow

---
+++ sparrow.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Sparrow < Formula
+    desc "sparrow项目资源下载工具"
+    homepage "https://codecenter.iqilu.com:8181/wyq/go-sparrow.git"
+    url "https://codecenter.iqilu.com:8181/wyq/go-sparrow/-/raw/main/sparrow_0.0.1.tar.gz"
+    sha256 "a4eed20e1f789def467061c9934e1d8265eb23ce313ec54029392ac9b0738e4b"
+    license "MIT"
+  
+    # depends_on "cmake" => :build
+  
+    def install
+      # ENV.deparallelize  # if your formula fails when building in parallel
+      # Remove unrecognized options if warned by configure
+      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+      # system "./configure", *std_configure_args, "--disable-silent-rules"
+      # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+      bin.install "sparrow"
+    end
+  
+    test do
+      # `test do` will create, run in and delete a temporary directory.
+      #
+      # This test will fail and we won't accept that! For Homebrew/homebrew-core
+      # this will need to be a test that verifies the functionality of the
+      # software. Run the test with `brew test sparrow`. Options passed
+      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+      #
+      # The installed folder is not in the path, so use the entire path to any
+      # executables being tested: `system "#{bin}/program", "do", "something"`.
+      system "false"
+    end
+  end
+  
